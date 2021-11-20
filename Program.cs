6aad7f95f6871cde103354519936ea2c6f1dc8c6
tejalwalimbe <tejalwalimbe2k@gmail.com>
2021-11-20 21:16:11 +0530

    UC1

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+namespace LineComparison
+    {
+        class Program
+
+        {
+            public static void Main(string[] args)
+            {
+                //user Input
+                Console.Write("Enter any  value x1: ");
+                double x1 = Convert.ToDouble(Console.ReadLine());
+                Console.Write("Enter any  value x2: ");
+                double x2 = Convert.ToDouble(Console.ReadLine());
+                Console.Write("Enter any  value y1: ");
+                double y1 = Convert.ToDouble(Console.ReadLine());
+                Console.Write("Enter any  value y2: ");
+                double y2 = Convert.ToDouble(Console.ReadLine());
+
+                //if else statement
+                if (x1 == x2 && y1 == y2)
+                {
+                    Console.WriteLine("Two lines are equal");
+                }
+                else
+                {
+                    Console.WriteLine("Two lines are not equal");
+                }
+
+
+                Console.ReadKey();
+            }
+        }
+
+
+    }
+
+
