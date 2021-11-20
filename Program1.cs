afbfd3f5e70e6bfc7c78d31ef47206af5d0ffae8
tejalwalimbe <tejalwalimbe2k@gmail.com>
2021-11-20 22:14:30 +0530

    UC4[TEJAL]

--- Program1.cs
+++ Program1.cs
@@ -19,21 +19,11 @@ namespace LineComparison
             Console.Write("Enter any  value y2: ");
             double y2 = Convert.ToDouble(Console.ReadLine());
 
-            double d1 = x1 - y1;
-            double d2 = x2 - y2;
 
-            int distance = d1.CompareTo(d2);
-
-            // check status
-            //use if else if statement
-            if (distance > 0)
-                Console.WriteLine("line 1 is greater than line 2");
-            else if (distance < 0)
-                Console.WriteLine("line 1 is less than line 2");
-            else
-                Console.WriteLine("line 1 is equal to line 2");
-
-
+            double distance = Convert.ToDouble(Console.Read());
+            //using distance formula to find out distance
+            distance = Math.Sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+            Console.WriteLine(distance);
             Console.ReadKey();
         }
     }
